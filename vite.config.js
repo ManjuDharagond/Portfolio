415033a10edfdfef382ed36b7131725bc605882e
ManjuDharagond <manjudmanju.dharagond@gmail.com>
2023-11-30 23:10:48 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio/"
+  base: "/Portfolio/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'three.js': ['three'],
+        },
+      },
+    },
+  },
 })
