5c10ad0cb97320f612f78368b7f6aaec321ec688
ManjuDharagond <manjudmanju.dharagond@gmail.com>
2023-12-01 00:41:08 +0530

    updating the base in vite.config

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
         <div className="relative z-0 bg-primary">
           <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
             <Navbar/>
-            {/* <Hero/> */}
+            <Hero/>
           </div>
           <About/>
           <Experience/>
@@ -14,7 +14,7 @@ const App = () => {
           <Works/>
           <Feedbacks/>
           <div className="relative z-0">
-              {/* <Contact/> */}
+              <Contact/>
               <StarsCanvas/>
           </div>
         </div>
